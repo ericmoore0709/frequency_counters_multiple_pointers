5b222cf98b0e3b32cb89b69b5d7c90cf7d1097a5
Eric Moore <ericmoore0709@gmail.com>
2024-10-22 15:56:27 -0700

    average-pair

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,18 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(intArr, targetAvg) {
+    if (!intArr.length) return false;
+
+    let ptr1 = 0;
+    let ptr2 = intArr.length - 1;
+
+    while (ptr1 < ptr2) {
+        const trueAvg = (intArr[ptr1] + intArr[ptr2]) / 2;
+        if (trueAvg === targetAvg) return true;
+        if (trueAvg < targetAvg) ptr1++;
+        else ptr2--;
+    }
+
+    return false;
 }
+
+module.exports = averagePair;
