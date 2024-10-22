1e34dcd1884140a3a2d22f9526bfd69bbc33d33a
Eric Moore <ericmoore0709@gmail.com>
2024-10-22 15:50:25 -0700

    construct-note

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,25 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+function constructNote(message, letters) {
+    if (!letters) return false;
+    if (!message) return true;
+
+    const messageMap = frequencyMap(message);
+    const lettersMap = frequencyMap(letters);
+
+    for (let letter in messageMap) {
+        if (!lettersMap[letter]) return false;
+        if (lettersMap[letter] < messageMap[letter]) return false;
+    }
+
+    return true;
 }
+
+function frequencyMap(str) {
+    const map = {};
+    for (let char of str) {
+        map[char] = map[char] + 1 || 1;
+    };
+    return map;
+}
+
+module.exports = constructNote;
