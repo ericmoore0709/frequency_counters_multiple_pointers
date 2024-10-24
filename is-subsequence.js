39c651bd6458fa959cfcb4a0bab087d662368a4f
Eric Moore <ericmoore0709@gmail.com>
2024-10-23 19:21:28 -0700

    is-subsequence

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,14 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(str1, str2) {
+    let ptr1 = 0;
+    let ptr2 = 0;
+
+    while (ptr1 < str1.length && ptr2 < str2.length) {
+        if (str1[ptr1] === str2[ptr2]) ptr1++;
+        ptr2++;
+    }
+
+    return (ptr1 === str1.length);
 }
+
+module.exports = isSubsequence;
