1c30bfb34e483f6546b6e69abbc1f962957fd740
Eric Moore <ericmoore0709@gmail.com>
2024-10-23 19:34:18 -0700

    count-pairs

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,16 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(arr, target) {
+    if (!arr.length) return 0;
+
+    let seen = new Set();
+    let count = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (seen.has(target - arr[i])) count++;
+        seen.add(arr[i]);
+    }
+
+    return count;
 }
+
+module.exports = countPairs;
